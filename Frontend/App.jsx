83e94c7fa7fe112eb53cf525e9cc39802bbfed56
Marion Balmonte <mflameion@gmail.com>
2026-02-08 17:36:52 +0800

    pics

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
     <NavigationContainer>
 
 
-      <Stack.Navigator initialRouteName="packages">
+      <Stack.Navigator initialRouteName="login">
         <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="signup" component={Signup} options={{ headerShown: false }} />
         <Stack.Screen name="home" component={Home} options={{ headerShown: false }} />
